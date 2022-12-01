3d311b0cea492064da31be4d566bde4c5ef10942
Elina Johansson <elina.johansson96@gmail.com>
2022-12-01 15:35:20 +0200

    eka commit

---
+++ harjoitusjs.js
@@ -0,0 +1,11 @@
+function laskuri() {
+    
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    
+    let sum = parseInt(number1) + parseInt(number2);
+    alert("Summa " + sum);
+}
+
+
+console.log("Hello HTML!");
